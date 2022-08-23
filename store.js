8e15dbbffa8b3a811662a9858a96e398750c4c13
Sana Hamayoon <sanahumayun5@gmail.com>
2022-08-23 18:08:50 +0500

    version 1

---
+++ store.js
@@ -0,0 +1,28 @@
+
+var removeCartItemButtons = document.getElementsByClassName('btn-danger')
+console.log(removeCartItemButtons)
+
+for (var i = 0; i < removeCartItemButtons.length; i++) {
+
+    var button = removeCartItemButtons[i]
+    button.addEventListener('click', function (event) {
+        var btnclicked = event.target
+        btnclicked.parentElement.parentElement.remove()
+        updateCartTotal()
+    })
+}
+function updateCartTotal() {
+    var cartItemContianer = document.getElementsByClassName('cart-items')[0]
+    var cartRows = cartItemContianer.getElementsByClassName('cart-row')
+    
+    var total = 0
+    for (var i = 0; i < cartRows.length; i++) {
+        var cartRow = cartRows[i]
+        var priceElement = cartRow.getElementsByClassName('cart-price')[0]
+        var quantityElement = cartRow.getElementsByClassName('cart-quantity-input')[0]
+        var price = parseFloat(priceElement.innerText.replace('$', ''))
+        var quantity = quantityElement.value
+        total = total + (price * quantity)   
+    }
+    document.getElementsByClassName('cart-total-price')[0].innerText = '$'+ total
+}
